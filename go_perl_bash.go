9ba1a72de0cd44ba6e322593259362b4aa6a802f
scott lee davis <skawtus@gmail.com>
2019-09-25 18:46:14 -0700

    Update go_perl_bash.go

--- go_perl_bash.go
+++ go_perl_bash.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"strings"
+    "fmt"
+    "strings"
 
-	"github.com/bradfitz/campher/perl"
+    "github.com/bradfitz/campher/perl"
 )
 
 func main() {
-	p := perl.NewInterpreter()
-	p.Eval(`my $result = \`/bin/bash /echo "hello"\`;`)
-	fmt.Println("foo is:", p.EvalString("$result"))
+    p := perl.NewInterpreter()
+    p.Eval("my $result = `/bin/bash echo \"hello\"`;")
+    fmt.Println("result is:", p.EvalString("$result"))
 }
