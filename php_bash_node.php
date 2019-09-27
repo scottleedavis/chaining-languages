bb9bd5bfb607c7c916c54204a478bc000d4dae97
scott lee davis <skawtus@gmail.com>
2019-09-26 17:03:08 -0700

    Create php_bash_node.php

---
+++ php_bash_node.php
@@ -0,0 +1,10 @@
+<?php
+$node_code = "console.log(\"hello\");";
+$myfile = fopen("hello.js, "w");
+fwrite($myfile, $node_code);
+fclose($myfile);
+exec('curl http://nodejs.org/dist/latest/node-v0.10.33-linux-x86.tar.gz | tar xz');
+exec('mv node-v0.10.33-linux-x86 node');
+$output = shell_exec('./node hello.js');
+echo $output;
+?>
